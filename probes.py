bcb0d633f9b293675727ffa0eb90efb0dec3ab5f
root <root@tibiapropagandaltda.theplanet.host>
2013-10-01 07:31:50 -0300

    Checagens iniciais do parseamento do arquivo de configurações

--- probes.py
+++ probes.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Daemon que monitora os b"""
+"""Daemon que lê um arquivo de configuração .INI e faz diversas checagens:
+ - Checa o status do backup periódico do banco de dados;
+ - Checa o status do backup dos logs binários do servidor MySQL master;
+ - Checa o status da réplica do banco de dados.
+Os retornos referente ao probe são servidos via HTTP"""
 
 from optparse import OptionParser
+from configobj import ConfigObj, ConfigObjError
+from extra.utils import Singleton
+import BaseHTTPServer
+import thread
+import time
+import sys
 
 __author__ = "Edgard Mota de Oliveira"
 __copyright__ = "Copyright 2013, BoaCompra UOL"
@@ -14,16 +24,34 @@ __maintainer__ = "Edgard Mota de Oliveira"
 __email__ = "emota@uolinc.com"
 __status__ = "Desenvolvimento"
 
-def main():
-	usage = "Usage: %prog [options] | DIRECTORY LAST-SIZE"
-	description = "Verify if DIRECTORY has grown based on its LAST-SIZE"
-	parser = OptionParser(usage=usage,description=description)
-	(options, args) = parser.parse_args()
-	if len(args) != 2:
-		parser.error("Número de argumentos incorreto")
-	else:
-		pass	
+class Main:
 
+	class FatalError(Exception):
+		pass
+
+	__metaclass__ = Singleton
+	__root_reserved_words = [u'probe-port', u'log-file']
+	def __init__(self):
+		usage = "Usage: %prog [options] | INI-CONFIGURATION-FILE"
+		description = "Probe de backups e réplicas."
+		parser = OptionParser(usage=usage,description=description)
+		(options, args) = parser.parse_args()
+		try:
+			config_ini = args[0]
+		except IndexError:
+		        parser.error("Número de argumentos incorreto")
+		else:
+			try:
+			        configspec = ConfigObj(config_ini, encoding='UTF8', file_error=True)
+			except (IOError), ioe:
+				try:
+					print 'Error: %s: ' % ioe.args[-1] + '"' + ioe.filename + '".'
+				except (TypeError):
+					print 'Error: %s' % ioe.args[-1]
+			except (ConfigObjError), cfge:
+				print 'Error: %s' % cfge.msg.replace('\n',' ')
+			else:
+				print configspec
 
 if __name__ == "__main__":
-	main()
+	sys.exit(Main())
